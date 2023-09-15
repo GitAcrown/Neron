82043e45011ccf686ae02f287c46e7e6b25afb21
GitAcrone <acrown@outlook.fr>
2023-09-15 17:24:30 +0200

    Correction bday next

--- cogs/birthdays/birthdays.py
+++ cogs/birthdays/birthdays.py
@@ -203,10 +203,8 @@ class Birthdays(commands.Cog):
             return await interaction.response.send_message("**Aucun anniversaire** · Aucun membre n'a défini de date d'anniversaire")
         
         today = datetime.now()
-        # Ceux de cette année
-        listebday = [(m, d.replace(year=today.year)) for m, d in birthdays.items() if d.month >= today.month and d.day >= today.day]
-        # Ceux de l'année prochaine
-        listebday += [(m, d.replace(year=today.year + 1)) for m, d in birthdays.items() if d.month < today.month or d.day < today.day]
+        # On trie les anniversaires par date en ajoutant un an si la date est passée
+        listebday = sorted([(m, d.replace(year=today.year + (1 if d < today else 0))) for m, d in birthdays.items()], key=lambda x: x[1].timestamp())
         
         if not listebday:
             return await interaction.response.send_message("**Aucun anniversaire** · Aucun anniversaire n'est prévu dans les prochains jours")
